3ea7bd7ddeb55009f49595599cd754528d07bcff
Fedor Zhulitov <fzhulitov@gmail.com>
2023-11-10 20:49:06 +0300

    Must work
    Looks complite

--- asus/tuf/tuf17fa707xv/default.nix
+++ asus/tuf/tuf17fa707xv/default.nix
@@ -2,11 +2,14 @@
 	imports = [
 		../../../common/cpu/amd
 		../../../common/cpu/amd/pstate.nix
-		../../../common/gpu/nvidia
+		../../../common/cpu/amd/raphael/igpu.nix
+		../../../common/gpu/amd
+		../../../common/gpu/nvidia #now it is prime
 #		../../../common/gpu/nvidia/prime.nix
 		../../../common/pc/laptop
 		../../../common/pc/ssd
 		../../battery.nix
+		../../../common/hidpi
 	];
 
 	hardware.nvidia.prime = {
